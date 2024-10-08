9fccadc97b68af73ea367a1f60b1812085bff864
Ben Smith <112433581+ben-smith23@users.noreply.github.com>
2024-10-08 21:31:29 +0100

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -28,7 +28,7 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
       if (metadata.over) {
         self.message(false); // You lose
       } else if (metadata.won) {
-        self.message(true); // You win!
+        self.message(true); // Hells yeah baby!
       }
     }
 
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "Hells yeah baby!" : "Game over!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
